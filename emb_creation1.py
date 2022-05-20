cff6facd039ccaa48622afb268bfec95469e1602
Laura <lauratriglia18@gmail.com>
2022-05-20 14:08:15 +0200

    <Edit> successfully edit, the prediction works correctly

--- emb_creation1.py
+++ emb_creation1.py
@@ -8,29 +8,34 @@ import torchaudio
 from speechbrain.pretrained import EncoderClassifier
 from speaker_embeddings import EmbeddingsHandler
 
+
 def make_chunk(audio_file):
     file_name = os.path.basename(audio_file)
     dir_name = os.path.dirname(audio_file)
-    #print(file_name)
-    print(dir_name)
+    print(audio_file)
+    # print(file_name)
+    # print(dir_name)
     list_emb = []
     classifier = EncoderClassifier.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb")
     chunk_length = 1000  # pydub calculates in millisec
     chunks = make_chunks(audio_file, chunk_length)  # make chunks of 1 sec
 
     for i, chunk in enumerate(chunks):
-        chunk_name = './chunks/' + "chunk{0}.wav".format(i)
-        signal, fs = torchaudio.load(chunk_name)
+        chunk_name = './chunks/' + os.path.splitext(file_name)[0] + "_chunk{0}.wav".format(i)
+       # chunk.export(chunk_name, format="wav")
+        #print(chunk_name)
+        signal, fs = torchaudio.load(chunk)
         embeddings = classifier.encode_batch(signal)
         embeddings = embeddings.squeeze(axis=0)
         list_emb.append(embeddings.numpy())
+       # print(list_emb)
         # output_probs, score, index, text_lab = classifier.classify_batch(signal)
         # print(embeddings)
 
     with open(dir_name+'/chunk.npy', 'wb') as f:
         np.save(f, np.array(list_emb))
 
-    predict_speaker(embeddings)
+    #predict_speaker(embeddings)
     return embeddings
 
 
@@ -50,15 +55,24 @@ def predict_speaker(embeddings):
 
 main_dir = 'DB_Final'
 all_file_names = os.listdir(main_dir)
-print(all_file_names)
+
 
 for subdir in all_file_names:
     if os.path.isdir(main_dir + '/' + subdir):
         dir_names = os.listdir(main_dir + '/' + subdir)
+        print(subdir)
     for subdir1 in dir_names:
         if os.path.isdir(main_dir+'/'+subdir+'/'+subdir1):
             files = os.listdir(main_dir+'/'+subdir+'/'+subdir1)
-            # predict_speaker(np.load(main_dir+'/'+subdir+'/'+subdir1+'/'+'chunk.npy').squeeze()[0])
-            for file in files:
-                if '.wav' in file:
-                  make_chunk(main_dir+'/'+subdir+'/'+subdir1+'/'+file)
+            embs = np.load(main_dir+'/'+subdir+'/'+subdir1+'/'+'chunk.npy').squeeze()
+            # print(f"EMB outside: {embs}")
+            print(subdir1)
+            for emb in embs:
+                predict_speaker(emb)
+            #for file in files:
+            #     if '.wav' in file:
+            #      make_chunk(main_dir+'/'+subdir+'/'+subdir1+'/'+file)
+
+#emb = np.load(main_dir+'/'+'Giacomo'+'/'+'2022_01_10'+'/'+'chunk.npy').squeeze()
+#print(emb)
+#predict_speaker(emb)
